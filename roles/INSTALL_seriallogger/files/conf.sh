846ebd457e426e878b2e647232fdb8d88412a9ef
Sylvain LAFRASSE <sylvain.lafrasse@lapp.in2p3.fr>
2021-09-06 16:31:09 +0200

    Add custom tio deployment if any.

--- roles/INSTALL_seriallogger/files/conf.sh
+++ roles/INSTALL_seriallogger/files/conf.sh
@@ -1,13 +1,13 @@
 #/bin/sh
 
-tio="$HOME/tio/src/tio"
+tio="$HOME/tio-master/src/tio"
 
 dest="$HOME/Logs/Test_MMC_v71"
 
-name=("IPMC" "IOIF" "AMC2" "AMC3")
+name=("IPMC" "IOIF" "AMC1" "AMC2" "AMC3" "AMC4")
 
 dev_prefix="/dev/serial/by-id/usb-FTDI_TTL232R-3V3_"
 dev_suffix="-if00-port0"
-dev=("${dev_prefix}FTHBSUJF${dev_suffix}" "${dev_prefix}FTHBVLFH${dev_suffix}" "${dev_prefix}FTAK2D0W${dev_suffix}" "${dev_prefix}FTHBSX4H${dev_suffix}")
+dev=("${dev_prefix}FTHBSUJF${dev_suffix}" "${dev_prefix}FTHBVLFH${dev_suffix}" "${dev_prefix}FTBRZJY3${dev_suffix}" "${dev_prefix}FTBS0YM9${dev_suffix}" "${dev_prefix}FTFMFDFT${dev_suffix}" "${dev_prefix}FTAK2D0W${dev_suffix}")
 
-baud=("115200" "115200" "19200" "19200")
+baud=("115200" "115200" "19200" "19200" "19200" "19200")
