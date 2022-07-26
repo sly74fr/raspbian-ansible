4a03ddd5be2a9642791e40209829db70f81a0664
Sylvain LAFRASSE <sylvain.lafrasse@lapp.in2p3.fr>
2022-07-26 13:52:16 +0200

    Update tio args for version 1.48.

--- roles/INSTALL_seriallogger/files/start_all.sh
+++ roles/INSTALL_seriallogger/files/start_all.sh
@@ -86,7 +86,7 @@ do
         #exit 5
     else
         file=$full/$n.log
-        screen -d -S $n -m $tio -b $b -t -l $file $d
+        screen -d -S $n -m $tio -b $b $d -c none -t --timestamp-format=iso8601 --log-strip --log-file=$file 
         if [ $? -ne 0 ]
         then
             exit 6
