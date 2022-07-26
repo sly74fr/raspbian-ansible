6a7fbabcba8b05eaf5a892fdfd2ed5c05511c028
Sylvain LAFRASSE <sylvain.lafrasse@lapp.in2p3.fr>
2022-07-26 13:39:49 +0200

    Enforce shell script robustness with usual execution settings.

--- roles/INSTALL_seriallogger/files/start_all.sh
+++ roles/INSTALL_seriallogger/files/start_all.sh
@@ -1,5 +1,10 @@
 #/bin/sh
 
+set -e  # Abort script at first error, when a command exits with non-zero status (except in until or while loops, if-tests, list constructs)
+set -u  # Attempt to use undefined variable outputs error message, and forces an exit
+set -o pipefail  # Causes a pipeline to return the exit status of the last command in the pipe that returned a non-zero return value.
+### set -x  # Similar to verbose mode (-v), but expands commands
+
 tio="./TIO/build/src/tio"
 ### tio=`which tio`
 joker=""
