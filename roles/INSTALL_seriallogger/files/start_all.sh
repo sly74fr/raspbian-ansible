ecbcb57762ac723c2cdcf3e156c124d38b13cc9a
Sylvain LAFRASSE <sylvain.lafrasse@lapp.in2p3.fr>
2026-01-28 16:27:16 +0100

    Disable shell debugging settings.

--- roles/INSTALL_seriallogger/files/start_all.sh
+++ roles/INSTALL_seriallogger/files/start_all.sh
@@ -1,40 +1,43 @@
-#/bin/sh
+#/bin/bash
 
-set -e  # Abort script at first error, when a command exits with non-zero status (except in until or while loops, if-tests, list constructs)
-set -u  # Attempt to use undefined variable outputs error message, and forces an exit
-set -o pipefail  # Causes a pipeline to return the exit status of the last command in the pipe that returned a non-zero return value.
+### WARNING: 'set -e' causes 'SIZE=`expr $NAME_SIZE - 1`' to stop execution unexepectidly, because it does not return 0 !!!
+
+### set -e  # Abort script at first error, when a command exits with non-zero status (except in until or while loops, if-tests, list constructs)
+### set -u  # Attempt to use undefined variable outputs error message, and forces an exit
+### set -o pipefail  # Causes a pipeline to return the exit status of the last command in the pipe that returned a non-zero return value.
 ### set -x  # Similar to verbose mode (-v), but expands commands
 
-tio="./TIO/build/src/tio"
-### tio=`which tio`
-joker=""
+### TIO=`which tio`
+TIO=~/Serial_Logger/TIO/build/src/tio
+JOKER=""
 
 # Read configuration file to overload previous variables
 source $1 # Configuration file
 if [ $? -ne 0 ]
 then
+    echo "ERROR> no configuration file found."
     exit 1
 fi
 
 # Check tio availability
-$tio --version > /dev/null 2>&1
+$TIO --version > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
     echo "ERROR> tio not found at '$tio' !"
     exit 2
 fi
-echo -n "$tio "
-$tio --version | head -1 | cut -d ' ' -f 2
+echo -n "$TIO "
+$TIO --version | head -1 | cut -d ' ' -f 2
 
 # Test LOCK file existance first
-locker="$dest/LOCK"
-if [ -f $locker ]
+LOCKER="$DEST/LOCK"
+if [ -f $LOCKER ]
 then
     echo "ERROR: a test is already ongoing !!!"
-    if [ -s $locker ] # Display the message in the LOCK file if any
+    if [ -s $LOCKER ] # Display the message in the LOCK file if any
     then
         echo "========="
-        cat $locker
+        cat $LOCKER
         echo "========="
     fi
     echo "Please RESPONSIVLY use the stop_all.sh script first, then relaunch $0."
@@ -42,70 +45,71 @@ then
 fi
 
 # Assert configuration arrays coherence
-name_size=${#name[@]}
-if [ $name_size == 0 ]
+NAME_SIZE=${#NAME[@]}
+if [ $NAME_SIZE == 0 ]
 then
     echo "ERROR> name array is empty !"
     exit 3
 fi    
-dev_size=${#dev[@]}
-if [ $name_size != $dev_size ]
+DEV_SIZE=${#DEV[@]}
+if [ $NAME_SIZE != $DEV_SIZE ]
 then
     echo "ERROR> arrays size mismatch !"
     exit 4
 fi    
-baud_size=${#baud[@]}
-if [ $baud_size != $dev_size ]
+BAUD_SIZE=${#BAUD[@]}
+if [ $BAUD_SIZE != $DEV_SIZE ]
 then
     echo "ERROR> arrays size mismatch !"
     exit 5
 fi    
 
 # Create new directory
-now=`date "+%Y.%m.%d.%H.%M.%S"`
-full="$dest/$now/"
-echo $full
-mkdir -p ${full}
+NOW=`date "+%Y.%m.%d.%H.%M.%S"` 
+FULL="$DEST/$NOW"
+echo $FULL
+mkdir -p ${FULL}
 if [ $? -ne 0 ]
 then
     exit 6
 fi
-ln -sfn $full $dest/CURRENT # Update CURRENT symlink to newly created directory
+ln -sfn $FULL $DEST/CURRENT # Update CURRENT symlink to the newly created directory
 
 # Start screened tio
-size=`expr $name_size - 1`
-for i in `seq 0 $size`
+SIZE=`expr $NAME_SIZE - 1`
+for i in `seq 0 $SIZE`
 do
-    n=`eval echo \${name[$i]}`
-    d=`eval echo \${dev[$i]}`
-    b=`eval echo \${baud[$i]}`
+    N=`eval echo \${NAME[\$i]}`
+    D=`eval echo \${DEV[\$i]}`
+    B=`eval echo \${BAUD[\$i]}`
 
-    if [ ! -c $d ]
+    if [ ! -c $D ]
     then
-        echo "WARNING> skipping device '$d' not found !"
+        echo "WARNING> skipping device '$D' not found !"
     else
-        file=$full/$n.log
-        screen -d -S $n -m $tio -b $b $d -c none -t --timestamp-format=iso8601 --log-strip --log-file=$file 
+        FILE=$FULL/$N.log
+        screen -d -S $N -m $TIO -b $B $D -c none -t --timestamp-format=iso8601 -l --log-strip --log-file=$FILE
         if [ $? -ne 0 ]
         then
+            echo "ERROR> could not start '$N' logger."
             exit 7
         fi
-        echo "Logging '$n' ($d) at $b bauds in '$file'."
+        echo "Logging '$N' ($D) at $B bauds in '$FILE'."
     fi
 done
 
-# Start sudoed screened joker
-if ! test -z "$joker"
+# Start screened JOKER if any
+if ! test -z "$JOKER"
 then
-    file=$full/JOKER.log
-    sudo screen -d -S JOKER -L -Logfile $file -m $joker
+    FILE=$FULL/JOKER.log
+    sudo screen -d -S JOKER -L -Logfile $FILE -m $JOKER
     if [ $? -ne 0 ]
     then
         exit 8
     fi
-    echo "Logging '$joker' in '$file'."
+    echo "Logging '$JOKER' in '$FILE'."
 fi
 
-touch $locker
+touch $LOCKER
 
 exit 0
--- roles/INSTALL_seriallogger/files/stop_all.sh
+++ roles/INSTALL_seriallogger/files/stop_all.sh
@@ -1,11 +1,13 @@
-#/bin/sh
+#/bin/bash
 
-set -e  # Abort script at first error, when a command exits with non-zero status (except in until or while loops, if-tests, list constructs)
-set -u  # Attempt to use undefined variable outputs error message, and forces an exit
-set -o pipefail  # Causes a pipeline to return the exit status of the last command in the pipe that returned a non-zero return value.
+### WARNING: 'set -e' causes 'SIZE=`expr $NAME_SIZE - 1`' to stop execution unexepectidly, because it does not return 0 !!!
+
+### set -e  # Abort script at first error, when a command exits with non-zero status (except in until or while loops, if-tests, list constructs)
+### set -u  # Attempt to use undefined variable outputs error message, and forces an exit
+### set -o pipefail  # Causes a pipeline to return the exit status of the last command in the pipe that returned a non-zero return value.
 ### set -x  # Similar to verbose mode (-v), but expands commands
 
-joker=""
+JOKER=""
 
 # Read configuration file to overload previous variables
 source $1 # Configuration file
@@ -15,34 +17,36 @@ then
 fi
 
 # Test LOCK file existance first
-locker="$dest/LOCK"
-if [ ! -f $locker ]
+LOCKER="$DEST/LOCK"
+if [ ! -f $LOCKER ]
 then
     echo "No LOCK file found, nothing to stop then !"
     exit 0
 fi
 
-name_size=${#name[@]}
-if [ $name_size == 0 ]
+NAME_SIZE=${#NAME[@]}
+if [ $NAME_SIZE == 0 ]
 then
     echo "ERROR> name array is empty !"
     exit 2
-fi    
-size=`expr $name_size - 1`
-for i in `seq 0 $size`
+fi
+echo OK
+
+SIZE=`expr $NAME_SIZE - 1`
+for i in `seq 0 $SIZE`
 do
-    n=`eval echo \${name[$i]}`
-    echo "Stopping '$n' log."
-    screen -X -S $n quit >/dev/null 2>&1 || true
+    N=`eval echo \${NAME[\$i]}`
+    echo "Stopping '$N' log."
+    screen -X -S $N quit >/dev/null 2>&1 || true
 done
 
-if ! test -z "$joker"
+if ! test -z "$JOKER"
 then
     echo "Stopping 'JOKER' log."
     sudo screen -X -S JOKER quit >/dev/null 2>&1 || true
 fi
 
-rm -f $locker
+rm -f $LOCKER
 echo "LOCK file removed."
 
 exit 0
